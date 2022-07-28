eee5f23703e5244f92c5a62638185afdcc5c0e83
hoangdanh <hoangdanh19xx@gmail.com>
2022-07-28 16:07:27 +0700

    add appbar & boardbar interface

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,8 +1,78 @@
 import React from "react";
+import BootstrapContainer from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+
 import "./AppBar.scss";
 
 function AppBar() {
-  return <nav className="navbar-app">AppBar</nav>;
+  return (
+    <nav className="navbar-app">
+      <BootstrapContainer className="trello-container">
+        <Row>
+          <Col sm={5} xs={12} className="col-no-padding">
+            <div className="app-actions">
+              <div className="item all">
+                <i className="fa-brands fa-themeco"></i>
+              </div>
+              <div className="item home">
+                <i className="fa-solid fa-house"></i>
+              </div>
+              <div className="item boards">
+                <i className="fa-solid fa-line-columns"></i>&nbsp;&nbsp;
+                <strong>Boards</strong>
+              </div>
+              <div className="item search">
+                <InputGroup className="group-search">
+                  <Form.Control
+                    className="input-search"
+                    placeholder="Jump to..."
+                  />
+                  <InputGroup.Text className="input-icon-search">
+                    <i className="fa-solid fa-magnifying-glass"></i>
+                  </InputGroup.Text>
+                </InputGroup>
+              </div>
+            </div>
+          </Col>
+          <Col sm={2} xs={12} className="col-no-padding">
+            <div className="app-branding text-center">
+              <a href="!#" target="_blank">
+                <img
+                  src="https://images.unsplash.com/photo-1658830123727-4febcf91ec80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0MHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+                  alt="trello-logo"
+                  className="top-logo"
+                />
+                <span className="trello-slogan">trello-clone</span>
+              </a>
+            </div>
+          </Col>
+          <Col sm={5} xs={12} className="col-no-padding">
+            <div className="user-actions">
+              <div className="item quick">
+                <i className="fa-solid fa-plus"></i>
+              </div>
+              <div className="item news">
+                <i className="fa-solid fa-circle-info"></i>
+              </div>
+              <div className="item notification">
+                <i className="fa-solid fa-bell"></i>
+              </div>
+              <div className="item user-avatar">
+                <img
+                  src="https://images.unsplash.com/photo-1644982647711-9129d2ed7ceb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxNDN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt="avatar-dev"
+                  title="avatar"
+                />
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </BootstrapContainer>
+    </nav>
+  );
 }
 
 export default AppBar;
--- src/components/BoardBar/BoardBar.js
+++ src/components/BoardBar/BoardBar.js
@@ -1,8 +1,64 @@
 import React from "react";
 import "./BoardBar.scss";
 
+import BootstrapContainer from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 function BoardBar() {
-  return <nav className="navbar-board">BoardBar</nav>;
+  return (
+    <nav className="navbar-board">
+      <BootstrapContainer className="trello-container">
+        <Row>
+          <Col sm={10} xs={12} className="col-no-padding">
+            <div className="board-info">
+              <div className="item board-logo-icon">
+                <i className="fa-solid fa-coffee-bean"></i>&nbsp;&nbsp;
+                <strong>MERNSTACK</strong>
+              </div>
+              <div className="divider"></div>
+
+              <div className="item board-type">Private Workspace</div>
+              <div className="divider"></div>
+
+              <div className="item member-avatar">
+                <img
+                  src="https://images.unsplash.com/photo-1658786422874-f36281333baa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNDl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt="avatar"
+                />
+                <img
+                  src="https://images.unsplash.com/photo-1658848588646-58417ec28548?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNTZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt="avatar"
+                />
+                <img
+                  src="https://images.unsplash.com/photo-1657299143322-934f44698807?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxNjF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt="avatar"
+                />
+                <img
+                  src="https://images.unsplash.com/photo-1658786422874-f36281333baa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNDl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt="avatar"
+                />
+                <img
+                  src="https://images.unsplash.com/photo-1658829839256-aa4ed17b0622?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNzd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt="avatar"
+                />
+                <span className="more-members">+7</span>
+                <span className="invite">Invite</span>
+              </div>
+            </div>
+          </Col>
+          <Col sm={2} xs={12} className="col-no-padding">
+            <div className="board-actions">
+              <div className="item menu">
+                <i class="fa-solid fa-ellipsis"></i>
+                Show menu
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </BootstrapContainer>
+    </nav>
+  );
 }
 
 export default BoardBar;
